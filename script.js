d16608ce1e222cd16f2a1230d60fb670554e07c0
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-28 15:22:38 +0200

    added the cookies message

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,9 +18,8 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,21 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+ 
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
+const header = document.querySelector('.header')
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'we use cookies to improve functionalities and analytics';
+
+message.innerHTML = 'we use cookies to improve functionalities and analytics.<button class="btn btn--close-cookies">Got it!</button>'
+header.append(message);
+
+// header.append(message.cloneNode(true))
+
+document.querySelector('.btn--close-cookies').addEventListener(
+  'click', function() {
+    message.remove()
+  }
+)

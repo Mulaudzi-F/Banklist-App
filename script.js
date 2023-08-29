2bf5afe3edcbc5120f2f6a455db9713f27729e82
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-29 04:59:48 +0200

    completed smoth scrolling for 1 link

--- script.js
+++ script.js
@@ -30,20 +30,46 @@ document.addEventListener('keydown', function (e) {
   }
 });
  
-const allSections = document.querySelectorAll('.section');
-const allButtons = document.getElementsByTagName('button');
-const header = document.querySelector('.header')
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-//message.textContent = 'we use cookies to improve functionalities and analytics';
+// const allSections = document.querySelectorAll('.section');
+// const allButtons = document.getElementsByTagName('button');
+// const header = document.querySelector('.header')
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// //message.textContent = 'we use cookies to improve functionalities and analytics';
 
-message.innerHTML = 'we use cookies to improve functionalities and analytics.<button class="btn btn--close-cookies">Got it!</button>'
-header.append(message);
+// message.innerHTML = 'we use cookies to improve functionalities and analytics.<button class="btn btn--close-cookies">Got it!</button>'
+// header.append(message);
 
-// header.append(message.cloneNode(true))
+// // header.append(message.cloneNode(true))
 
-document.querySelector('.btn--close-cookies').addEventListener(
-  'click', function() {
-    message.remove()
-  }
-)
+// document.querySelector('.btn--close-cookies').addEventListener(
+//   'click', function() {
+//     message.remove()
+//   }
+// )
+
+
+// smoth scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e){
+    //getting coordinates of element we want to scrol to
+  const s1coords = section1.getBoundingClientRect() 
+   console.log(e.target.getBoundingClientRect())
+
+   // getting current score position
+   
+
+   //now scrolling
+   // exampl31 window.scrollTo(s1coords.left + window.PageXoffset, s1coords.top + window.PageYoffset);
+
+   section1.scrollIntoView({
+     behavior: 'smooth'
+   })
+  }) 
+
+  const h1 = document.querySelector('h1');
+  
+  h1.
